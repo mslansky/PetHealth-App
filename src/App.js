a3665716607b3b5a8d57310dd5340ab21639dca6
Madison Slansky <madisonslansky@Madisons-Air.attlocal.net>
2020-12-14 15:31:25 -0600

    initial landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <ul>
+      <li><a className="active" href="#home">PetHealth</a></li>
+      <li><a href="#contact">Tracker</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
+
+    <div className="intro-picture">
+    <img src="https://images.unsplash.com/photo-1526363269865-60998e11d82d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Man and Woman sitting on couch with dog and cat"/>
+    <div className="intro-text">Track Your Best Friend's Health</div> 
+    </div>
+
       </header>
     </div>
   );
