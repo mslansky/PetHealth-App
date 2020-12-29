35c11babf1e0f1e0a0a69cd689773ba3c8c8ea1a
Madison Slansky <madisonslansky@Madisons-MacBook-Air.local>
2020-12-29 14:51:58 -0700

    profile state created

--- src/profilepage/createprofile.js
+++ src/profilepage/createprofile.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import ProfileForm from './profileform';
-import './createprofile.css'
+import './createprofile.css';
+import config from '../config';
+import ApiContext from '../ApiContext';
+
 
 
 export default class Createprofile extends React.Component{
+  static defaultProps = {
+    history: {
+      push: () => { }
+    },
+  }
+  static contextType = ApiContext;
+
   handleSubmit = e => {
     e.preventDefault()
     const profile = {
       name: e.target['profile-name'].value
-    }}; 
+    }
+
+    fetch(`${config.API_ENDPOINT}/profiles`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${config.API_TOKEN}`
+      },
+      body: JSON.stringify(profile),
+    })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .then(profile => {
+        this.context.addProfile(profile)
+        this.props.history.push(`/profiles/${profile.id}`)
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+  }
 
   render(){
   return(
