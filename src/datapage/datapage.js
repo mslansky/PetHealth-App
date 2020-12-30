a2764a6929a1d6efad7915ef0ff8d8f0f458c296
Madison Slansky <madisonslansky@Madisons-MacBook-Air.local>
2020-12-30 16:39:54 -0700

    toggle fixed

--- src/datapage/datapage.js
+++ src/datapage/datapage.js
@@ -7,52 +7,60 @@ import ApiContext from '../ApiContext';
 export default class Datapage extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {medication: false, weight:false, diet:false, allergies:false, body:false, other:false,
-    };
+    this.state = {togglemedication: false, toggleweight:false, togglediet:false, toggleallergies:false, togglebody:false, toggleother:false,
+    diarydate:'', medication:'', weight:'', diet:'', allergies:'', body:'', other:''};
     this.toggleMedication = this.toggleMedication.bind(this);
     this.toggleWeight = this.toggleWeight.bind(this);
     this.toggleDiet = this.toggleDiet.bind(this);
     this.toggleAllergies = this.toggleAllergies.bind(this);
     this.toggleBody = this.toggleBody.bind(this);
     this.toggleOther = this.toggleOther.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.handleChangeDate = this.handleChangeDate.bind(this);
+    this.handleChangeMedication = this.handleChangeMedication.bind(this);
+    this.handleChangeWeight = this.handleChangeWeight.bind(this);
+    this.handleChangeDiet = this.handleChangeDiet.bind(this);
+    this.handleChangeAllergies = this.handleChangeAllergies.bind(this);
+    this.handleChangeBody = this.handleChangeBody.bind(this);
+    this.handleChangeOther = this.handleChangeOther.bind(this);
   }
 
     toggleMedication = ()=>{
       this.setState({
-        medication: !this.state.medication
+        togglemedication: !this.state.togglemedication
       })
   }
 
     toggleWeight = ()=>{
       this.setState({
-      weight: !this.state.weight
+        toggleweight: !this.state.toggleweight
       })
     }
 
     toggleDiet = ()=>{
       this.setState({
-      diet: !this.state.diet
+        togglediet: !this.state.togglediet
       })
     }
 
     toggleAllergies = ()=>{
       this.setState({
-      allergies: !this.state.allergies
+        toggleallergies: !this.state.toggleallergies
       })
     }
 
     toggleBody= ()=>{
       this.setState({
-      body: !this.state.body
+        togglebody: !this.state.togglebody
       })
     }
 
     toggleOther= ()=>{
       this.setState({
-      other: !this.state.other
+        toggleother: !this.state.toggleother
       })
     }
-
 
     // add diary
     static defaultProps = {
@@ -62,16 +70,46 @@ export default class Datapage extends React.Component{
     }
     static contextType = ApiContext;
 
+    handleChangeDate (event) {
+      this.setState({diarydate:event.target.value});
+    }
+
+    handleChangeMedication(event) {
+      this.setState({medication: event.target.value});
+    }
+
+    handleChangeWeight(event) {
+      this.setState({weight: event.target.value});
+    }
+
+    handleChangeDiet (event) {
+      this.setState({diet: event.target.value});
+    }
+
+    handleChangeAllergies (event) {
+      this.setState({allergies: event.target.value});
+    }
+
+    handleChangeBody (event) {
+      this.setState({body: event.target.value});
+    }
+
+    handleChangeOther (event) {
+      this.setState({other: event.target.value});
+    }
+
     handleSubmit = e => {
       e.preventDefault()
       const newDiary = {
-        medication: e.target['text-medication'].value,
-        weight: e.target['text-weight'].value,
-        diet: e.target['text-diet'].value,
-        allergies: e.target['text-allergies'].value,
-        body: e.target['text-body'].value,
-        other: e.target['text-other'].value,
+        diarydate: this.state.diarydate,
+        medication: this.state.medication,
+        weight: this.state.weight,
+        diet: this.state.diet,
+        allergies: this.state.allergies,
+        body: this.state.body,
+        other: this.state.other,
       }
+
       fetch(`${config.API_ENDPOINT}/diaries`, {
         method: 'POST',
         headers: {
@@ -85,9 +123,9 @@ export default class Datapage extends React.Component{
             return res.json().then(e => Promise.reject(e))
           return res.json()
         })
-        .then(diary => {
-          this.context.addDiary(diary)
-          this.props.history.push(`/profile/${diary.id}`)
+        .then(diaries => {
+          this.context.addDiary(newDiary)
+          this.props.history.push(`/profile/${diaries.id}`)
         })
         .catch(error => {
           console.error({ error })
@@ -95,19 +133,17 @@ export default class Datapage extends React.Component{
     }
 
 
-
-
   render(){
     return(
       <div className="datapage">
 
-        <h1>PET NAME HERE</h1>
+        <h1>NAME HERE</h1>
 
         <div className="data-inputs">
        
         <div className="input-val-cal">
         <label for="diarydate">Today's Date: </label>
-        <input type="date" id="diarydate" name="diarydate"></input>
+        <input type="date" id="diarydate" name="diarydate" onChange={this.handleChangeDate} value={this.state.diarydate}></input>
         </div>
         
         
@@ -116,8 +152,8 @@ export default class Datapage extends React.Component{
         <span onClick={this.toggleMedication}>+ medications</span>
         <div className="input-val">
         { (() => {
-            if(this.state.medication){
-              return (<input className="medication" id="text-medication" type="text"/>);
+            if(this.state.togglemedication){
+              return (<input className="medication" id="text-medication" type="text" onChange={this.handleChangeMedication} value={this.state.medication}/>);
             }
           })()}
         </div>
@@ -125,8 +161,8 @@ export default class Datapage extends React.Component{
         <span onClick={this.toggleWeight}>+ weight</span>
         <div className="input-val">
         { (() => {
-            if(this.state.weight){
-              return (<input className="weight" id="text-weight" type="text"/>);
+            if(this.state.toggleweight){
+              return (<input className="weight" id="text-weight" type="text" onChange={this.handleChangeWeight} value={this.state.weight}/>);
             }
           })()}
         </div>
@@ -134,8 +170,8 @@ export default class Datapage extends React.Component{
         <span onClick={this.toggleDiet}>+ diet</span>
         <div className="input-val">
         { (() => {
-            if(this.state.diet){
-              return (<input className="diet" id="text-diet" type="text"/>);
+            if(this.state.togglediet){
+              return (<input className="diet" id="text-diet" type="text" onChange={this.handleChangeDiet} value={this.state.diet}/>);
             }
           })()}
         </div>
@@ -144,8 +180,8 @@ export default class Datapage extends React.Component{
         <span onClick={this.toggleAllergies}>+ allergies</span>
         <div className="input-val">
         { (() => {
-            if(this.state.allergies){
-              return (<input className="allergies" id="text-allergies" type="text"/>);
+            if(this.state.toggleallergies){
+              return (<input className="allergies" id="text-allergies" type="text" onChange={this.handleChangeAllergies} value={this.state.allergies}/>);
             }
           })()}
         </div>
@@ -153,8 +189,8 @@ export default class Datapage extends React.Component{
         <span onClick={this.toggleBody}>+ body care</span>
         <div className="input-val">
         { (() => {
-            if(this.state.body){
-              return (<input className="body" id="text-body" type="text"/>);
+            if(this.state.togglebody){
+              return (<input className="body" id="text-body" type="text" onChange={this.handleChangeBody} value={this.state.body}/>);
             }
           })()}
         </div>
@@ -162,8 +198,8 @@ export default class Datapage extends React.Component{
         <span onClick={this.toggleOther}>+ other</span>
         <div className="input-val">
         { (() => {
-            if(this.state.other){
-              return (<input className="other" id="text-other" type="text"/>);
+            if(this.state.toggleother){
+              return (<input className="other" id="text-other" type="text" onChange={this.handleChangeOther} value={this.state.other}/>);
             }
           })()}
         </div>
@@ -173,7 +209,7 @@ export default class Datapage extends React.Component{
         </div>
 
         <div className="box-1">
-        <div className="btn btn-one">
+        <div className="btn btn-one" onClick={this.handleSubmit}>
         <span>SAVE DIARY ENTRY</span>
         </div>
         </div>
